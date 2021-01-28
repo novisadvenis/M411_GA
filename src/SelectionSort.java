afdc0488b815c536dcdaf3d1bdd457e90336b462
Kaushall Vimalarajah <v.kaushi4@gmail.com>
2021-01-28 15:02:09 +0100

    SelectionSort modified

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,61 +1,57 @@
 
 
 /**
- *
  * Beschreibung
  * Einfach und instabil
  * Sortieren durch direktes auswählen
  * best/worst case = O(n^2)
  * Anzahl Vergleiche = n(n-1)/2
  *
- * @version 1.0 vom 21.01.2021
  * @author Kaushall Vimalarajah
+ * @version 1.0 vom 21.01.2021
  */
 
-public class SelectionSort extends SortWerte{
+public class SelectionSort extends SortWerte {
+
+    private String name="SelectionSort";
+    public SelectionSort(){
+        super.setName(name);
+    }
 
     @Override
     public void logic(int[] arr, int length) {
 
-        long start = System.nanoTime();
-        double[] messArr;
-
-        double schleifen = 0;
-        double vergleiche = 0;
-
         int i, j, minValue, minIndex, temp = 0;
         for (i = 0; i < length; i++) {
-            vergleiche++;
+            incrementAnzahlVergleiche();
             minValue = arr[i];
             minIndex = i;
             for (j = i; j < length; j++) {
-                vergleiche++;
+                incrementAnzahleSchliefe();
                 if (arr[j] < minValue) {
 
                     minValue = arr[j];
                     minIndex = j;
-                    schleifen++;
+                    incrementAnzahleSchliefe();
                 }
-                vergleiche++;
-                schleifen++;
+                incrementAnzahlVergleiche();
+                incrementAnzahleSchliefe();
             }
             if (minValue < arr[i]) {
                 temp = arr[i];
                 arr[i] = arr[minIndex];
                 arr[minIndex] = temp;
-                schleifen++;
+                incrementAnzahleSchliefe();
             }
-            vergleiche++;
-            schleifen++;
+            incrementAnzahlVergleiche();
+            incrementAnzahleSchliefe();
         }
-        schleifen++;
+        incrementAnzahleSchliefe();
 
         incrementAnzahlVergleiche();
         incrementAnzahleSchliefe();
 
     }
-
-
 
 
 }
